0bc3f58cd76dc22e07b19699d5a1214bd659fbf4
mtsteo <mateusteotonho@gmail.com>
2024-11-28 22:45:29 -0300

    fix: fixing dynamic route error

--- app/api/elections/vote/check/route.ts
+++ app/api/elections/vote/check/route.ts
@@ -2,6 +2,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { clientDb } from "@/lib/mongodb";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     const email = request.nextUrl.searchParams.get("email");
