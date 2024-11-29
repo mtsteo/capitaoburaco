deb4a8fa5515239b1e8426cb644db7bf72c97744
mtsteo <mateusteotonho@gmail.com>
2024-11-28 23:13:39 -0300

    fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,4 +3,8 @@ import { MongoClient } from "mongodb";
 // Replace the uri string with your connection string.
 const uri = process.env.MONGODB_URI;
 
+if (!uri) {
+  throw new Error("A variável de ambiente MONGODB_URI não está definida.");
+}
+
 export const clientDb = new MongoClient(uri);
